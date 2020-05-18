f7228d1bd7e569a76cf36445830af68e0028f2bd
oldgit <jon.harvey.uk@gmail.com>
2020-05-18 13:18:49 +0100

    Add confirm dialog to show answers

--- assets/js/home-quiz.js
+++ assets/js/home-quiz.js
@@ -3,7 +3,9 @@ document
   .forEach(b => b.addEventListener('click', ev => {  
     let answers = ev.target.parentElement.nextElementSibling;
     if (answers.className === 'hide') {
-      answers.className = 'show';
+      if (window.confirm('Really show the answers?')) { 
+        answers.className = 'show';
+      }
     } else {
       answers.className = 'hide';
     }
